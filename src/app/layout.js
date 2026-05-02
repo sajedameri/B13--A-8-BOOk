32dd6f8330bb58fa9c7fb1ff0c6c17cd15515bd6
sajedameri <merisajeda@gmail.com>
2026-05-02 11:31:51 +0600

    banner part added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,13 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit} from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+
+
+const outfitFont = Outfit({
+  
   subsets: ["latin"],
 });
 
@@ -22,10 +21,14 @@ export default function RootLayout({ children }) {
     <html
       lang="en"
       data-theme="light"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${outfitFont.className} h-full antialiased`}
     >
     
-      <body className="min-h-full flex flex-col"><Navbar/>{children}  </body>
+      <body className="min-h-full flex flex-col">
+        <Navbar/>
+       
+      {children}  </body>
+      <Footer/>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
+import Banner from "@/components/Banner";
 import Image from "next/image";
 
 export default function Home() {
   return (
- <h1 className="text-red-500">Home page</h1>
+     <Banner/>
+
 
 
   );
